7eb3afced0384c2b9401af7847e1eb1f97f776ce
aniketkesarkar <aniketkesarkar1020@gmail.com>
2026-07-02 06:08:21 +0000

    terraform module to create resource group

---
+++ terraform-modules/modules/resource-group/main.tf
@@ -0,0 +1,9 @@
+#inside main.tf we are going to add resource block 
+  resource "azurerm_resource_group" "myrg" {
+  name = "${var.resource_name_prefix}-${var.name}" #sap-dev-gopal-rg
+  #business division + environment + name of the resource group
+  location = var.location
+  tags = var.tags
+
+}
+
